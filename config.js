e647855ad89d2171a031e1cfaab28f7b1120aaca
long1812 <95009221+long1812@users.noreply.github.com>
2021-11-25 16:55:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    titleWeb: "Chào em",
-    introTitle: 'Chào em🥰',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Yêu Em",
+    introTitle: 'Chào bạn🥰',
+    introDesc: `Không biết cảm giác
+    khi yêu một người
+    sẽ như thế nào nhỉ :))`,
     btnIntro: 'hihi❤️',
-    title: 'Làm người yêu anh nhé 🥰',
-    desc: 'Khum trả lời hoặc thoát ra thì em là ngy anh đấyyy :> ',
+    title: 'Làm người yêu Long nhé 🥰',
+    desc: 'Khum trả lời hoặc thoát ra thì bạn là ngiu Long đấyyy :>> ',
     btnYes: 'Đồng ý lunn <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu mình Long thoiii ❤️',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    btnReply: 'Gửi cho Longg <3',
+    reply: 'Tại vì Long đẹp zaiii ❤️',
+    mess: 'Ohhh cám ơn bạn 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100015646293297' 
