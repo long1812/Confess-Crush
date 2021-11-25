13e94ab447bbc95fb5afdff7d5f803ee7a16d14a
long1812 <95009221+long1812@users.noreply.github.com>
2021-11-25 16:37:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Chào em",
-    introTitle: 'Yêu anh khum🥰',
+    introTitle: 'Chào em🥰',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
